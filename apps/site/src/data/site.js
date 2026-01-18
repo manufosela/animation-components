2055a5da926ce40bb96c048900b78c6c7b8f2c91
manufosela <manufosela@gmail.com>
2026-01-18 12:08:40 +0100

    Add Astro catalog and Pages workflow

---
+++ apps/site/src/data/site.js
@@ -0,0 +1,6 @@
+export const siteConfig = {
+  title: "Animation Components",
+  description: "Animated web components with rich demos and example setups.",
+  repoUrl: "https://github.com/manufosela/animation-components",
+  basePath: "/animation-components/",
+};
